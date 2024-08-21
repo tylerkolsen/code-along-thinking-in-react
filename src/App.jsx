28f6235105c27af7110c423190a688dfd415e537
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-21 11:24:28 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const TEST_DATA = [
+  { id: 0, description: 'Content plan', rate: 50, hours: 4 },
+  { id: 1, description: 'Copy writing', rate: 50, hours: 2 },
+  { id: 2, description: 'Website design', rate: 50, hours: 5 },
+  { id: 3, description: 'Website development', rate: 100, hours: 5 },
+];
+
+function App() {
+  return <p>FIXME</p>;
+}
+
+export default App;
---
+++ src/components/InvoiceTable.jsx
@@ -0,0 +1,1 @@
+import './InvoiceTable.css';
